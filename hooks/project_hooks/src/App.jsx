dff9f0266eb98faf0956be4650230f3f493214ad
mahimasahu3 <mahimasahu30830@gmail.com>
2024-05-08 19:19:09 +0530

    mahima first commit

---
+++ hooks/project_hooks/src/App.jsx
@@ -0,0 +1,12 @@
+import InputComponent from './hook1';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <InputComponent />
+    </div>
+  );
+}
+
+export default App;
