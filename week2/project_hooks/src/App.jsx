3a3534abbbce21abdb507b8698bf35e08070fd02
mahimasahu3 <mahimasahu30830@gmail.com>
2024-05-11 18:32:45 +0530

    commit

--- week2/project_hooks/src/App.jsx
+++ week2/project_hooks/src/App.jsx
@@ -7,16 +7,16 @@ import { Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div> 
-	  <nav>
-      <Link to="/" className="nav-item">Homepage</Link>
-      <Link to="/about" className="nav-item">About Little Lemon</Link>
-      <Link to="/contact" className="nav-item">Contact</Link>
-	  </nav>
-  <Routes> 
+	    <nav>
+        <Link to="/" className="nav-item">Homepage</Link>
+        <Link to="/about" className="nav-item">About Little Lemon</Link>
+        <Link to="/contact" className="nav-item">Contact</Link>
+	    </nav>
+      <Routes> 
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/about" element={<AboutLittleLemon />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
-    </Routes>
+      </Routes>
     </div>
   );
 };
