441cddd5d3b78fdf860d6d81557e7aad0a8d6ce5
mahimasahu3 <mahimasahu30830@gmail.com>
2024-05-11 18:28:21 +0530

    commit

--- week2/project_hooks/src/App.jsx
+++ week2/project_hooks/src/App.jsx
@@ -1,12 +1,24 @@
-import InputComponent from './hook1';
-import './App.css';
+import "./App.css";
+import Homepage from "./HomePage";
+import AboutLittleLemon from "./AboutLittleLemon";
+import Contact from "./Contact";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <InputComponent />
+    <div> 
+	  <nav>
+      <Link to="/" className="nav-item">Homepage</Link>
+      <Link to="/about" className="nav-item">About Little Lemon</Link>
+      <Link to="/contact" className="nav-item">Contact</Link>
+	  </nav>
+  <Routes> 
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/about" element={<AboutLittleLemon />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+    </Routes>
     </div>
   );
-}
+};
 
 export default App;
