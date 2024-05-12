a2ee741ddb6d7e8894845f6d033a1d28d416f6f5
mahimasahu3 <mahimasahu30830@gmail.com>
2024-05-12 19:59:23 +0530

    commit

--- week2/project_hooks/src/HomePage.jsx
+++ week2/project_hooks/src/HomePage.jsx
@@ -1,11 +1,13 @@
-import ReactPlayer from "react-player/youtube";
+import ReactPlayer from "react-player";
 import butterfly from './assets/images/butterfly.jpg';
+import audio1 from './components/audio1';
 function Homepage(){
     return(
     <div>
         <h1>welcome home</h1>
         <img src={butterfly} height={200} alt="butterfly image"/>
-        <ReactPlayer url='https://www.youtube.com/watch?v=sfun_Asm9YY' />
+        <ReactPlayer url='https://www.youtube.com/watch?v=sfun_Asm9YY' playing={false}/>
+        <audio1/>
     </div>  
     );
 }
